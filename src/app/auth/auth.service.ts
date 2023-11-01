9cdda0b2c597b23183434ea4aa4f55ebaed06990
Brunelo <63827347+RFBnl@users.noreply.github.com>
2023-11-01 16:43:37 -0500

    files

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,179 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, from, map, tap } from 'rxjs';
+import { User } from './user.model';
+import { HttpClient } from '@angular/common/http';
+import { Preferences } from '@capacitor/preferences';
+import { environment } from 'src/environments/environment';
+
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  localId: string;
+  expiresIn: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService implements OnDestroy {
+  private _user = new BehaviorSubject<User | null>(null);
+  private activeLogoutTimer: any;
+
+  get userIsAuthenticated() {
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
+    );
+  }
+
+  get userId() {
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+
+  get token() {
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.token;
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+
+  constructor(private http: HttpClient) { }
+
+  autoLogin() {
+    return from(Preferences.get({ key: 'authData' })).pipe(
+      map(storedData => {
+        if (!storedData || !storedData.value) {
+          return null;
+        }
+        const parsedData = JSON.parse(storedData.value) as {
+          token: string;
+          tokenExpirationDate: string;
+          userId: string;
+          email: string;
+        };
+        const expirationTime = new Date(parsedData.tokenExpirationDate);
+        if (expirationTime <= new Date()) {
+          return null;
+        }
+        const user = new User(
+          parsedData.userId,
+          parsedData.email,
+          parsedData.token,
+          expirationTime
+        );
+        return user;
+      }),
+      tap(user => {
+        if (user) {
+          this._user.next(user);
+          this.autoLogout(user.tokenDuration);
+        }
+      }),
+      map(user => {
+        return !!user;
+      })
+    );
+  }
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${environment.apiQualifying
+        }`,
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(tap(this.setUserData.bind(this)));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${environment.apiQualifying
+        }`,
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(tap(this.setUserData.bind(this)));
+  }
+
+  logout() {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+    this._user.next(null);
+    Preferences.remove({ key: 'authData' });
+  }
+
+  ngOnDestroy() {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+  }
+
+  private autoLogout(duration: number) {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+    this.activeLogoutTimer = setTimeout(() => {
+      this.logout();
+    }, duration);
+  }
+
+  private setUserData(userData: AuthResponseData) {
+    const expirationTime = new Date(
+      new Date().getTime() + +userData.expiresIn * 1000
+    );
+    const user = new User(
+      userData.localId,
+      userData.email,
+      userData.idToken,
+      expirationTime
+    );
+    this._user.next(user);
+    this.autoLogout(user.tokenDuration);
+    this.storeAuthData(
+      userData.localId,
+      userData.idToken,
+      expirationTime.toISOString(),
+      userData.email
+    );
+  }
+
+  private storeAuthData(
+    userId: string,
+    token: string,
+    tokenExpirationDate: string,
+    email: string
+  ) {
+    const data = JSON.stringify({
+      userId: userId,
+      token: token,
+      tokenExpirationDate: tokenExpirationDate,
+      email: email
+    });
+    Preferences.set({ key: 'authData', value: data });
+  }
+}
+
